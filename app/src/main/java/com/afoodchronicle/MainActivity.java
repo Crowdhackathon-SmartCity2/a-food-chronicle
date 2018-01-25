64ff51be78d1e1414924f0b65dd057c631f366b5
Michał Diner <dinermichal@gmail.com>
2018-01-25 15:17:38 +0200

    added markers

--- app/src/main/java/com/afoodchronicle/MainActivity.java
+++ app/src/main/java/com/afoodchronicle/MainActivity.java
@@ -1,6 +1,7 @@
 package com.afoodchronicle;
 
 import android.app.FragmentManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,19 +23,39 @@ import com.afoodchronicle.Fragments.ImportFragment;
 import com.afoodchronicle.Fragments.MainFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
     SupportMapFragment sMapFragment;
+
     GoogleMap m_map;
+
     boolean mapReady=false;
+
+    MarkerOptions yoleni;
+    private static final LatLng mYoleni = new LatLng(37.9776514, 23.7388241);
+
+    MarkerOptions vorria;
+    private static final LatLng mVorria = new LatLng(37.9797024, 23.7281983);
+
+    MarkerOptions pnyka;
+    private static final LatLng mPnyka = new LatLng(37.9685393, 23.7478882);
+
+    MarkerOptions pantopoleio;
+    private static final LatLng mPantopoleio = new LatLng(38.0056227, 23.7826411);
+
+    static final CameraPosition ATHENS = CameraPosition.builder()
+            .target(new LatLng(37.9838096, 23.7275388))
+            .zoom(12)
+            .build();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        sMapFragment = SupportMapFragment.newInstance();
 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -58,6 +79,16 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+
+        initializeMaps();
+
+    }
+
+    public void initializeMaps(){
+
+        sMapFragment = SupportMapFragment.newInstance();
+
         FragmentManager fm = getFragmentManager();
         fm.beginTransaction().replace(R.id.content_frame, new MainFragment()).commit();
 
@@ -65,12 +96,14 @@ public class MainActivity extends AppCompatActivity
 
         android.support.v4.app.FragmentManager sFm = getSupportFragmentManager();
 
+
         if (!sMapFragment.isAdded())
             sFm.beginTransaction().add(R.id.map, sMapFragment).commit();
         else
             sFm.beginTransaction().show(sMapFragment).commit();
-    }
 
+
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -130,10 +163,39 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mapReady = true;
+
         m_map= googleMap;
-        LatLng athens = new LatLng(37.9838096, 23.7275388);
-        CameraPosition target = CameraPosition.builder().target(athens).zoom(12).build();
-        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+
+        m_map.moveCamera(CameraUpdateFactory.newCameraPosition(ATHENS));
+
+        // Add lots of markers to the map.
+        addMarkersToMap();
+
     }
-}
+
+    private void addMarkersToMap() {
+
+        yoleni = new MarkerOptions()
+                .position(mYoleni)
+                .title(getString(R.string.yoleni));
+        m_map.addMarker(yoleni);
+
+        vorria = new MarkerOptions()
+                .position(mVorria)
+                .title(getString(R.string.vorria));
+        m_map.addMarker(vorria);
+
+        pnyka = new MarkerOptions()
+                .position(mPnyka)
+                .title(getString(R.string.pnyka));
+        m_map.addMarker(pnyka);
+
+        pantopoleio = new MarkerOptions()
+                .position(mPantopoleio)
+                .title(getString(R.string.pantopoleio));
+        m_map.addMarker(pantopoleio);
+        }
+    }
+
